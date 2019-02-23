99f246824e901842bbc977703ab87c374fa4ffc9
Cương Nguyễn <cuong0993@gmail.com>
2019-02-23 06:12:59 +0300

    rename

--- android/src/main/java/com/chaomao/lala/AndroidLauncher.kt
+++ android/src/main/java/com/chaomao/lala/AndroidLauncher.kt
@@ -7,7 +7,7 @@ import android.widget.RelativeLayout
 import com.badlogic.gdx.backends.android.AndroidApplication
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
 
-class MainActivity : AndroidApplication() {
+class AndroidLauncher : AndroidApplication() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         platformObjects = PlatformObjectsImpl(this)
